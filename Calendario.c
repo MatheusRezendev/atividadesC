4194f54fd64c092cf293dcdc4d7add61e4761a3f
Matheus Rezende <118859608+MatheusRezendev@users.noreply.github.com>
2023-09-06 21:50:23 -0300

    Calendario
    Faça um programa que recebe do usuario o dia, mes e ano e calcule o proximo dia

---
+++ Calendario.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+
+    int dia, mes, ano;
+
+//Solicita as informações do usuario
+
+    printf("Digite o dia:\n");
+    scanf("%d", &dia);
+    if(dia < 0 || dia > 31){
+        printf("Opcao invalida\n");
+    }
+    printf("Digite o mes:\n");
+    scanf("%d", &mes);
+    if(mes < 0 || mes > 12){
+        printf("Opcao invalida\n");
+    }
+    printf("Digite o ano:\n");
+    scanf("%d", &ano);
+
+    int ano;
+    int resto;
+
+    ano= ("%d", &ano);
+    resto =ano / 4;
+
+    if(resto == 0, mes = 2, dia =29){
+    }
+
+
+
+
+
+}
