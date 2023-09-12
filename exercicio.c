1bb7a85020ac56625dfdd96899b229f1052f0e9a
Matheus Rezende <118859608+MatheusRezendev@users.noreply.github.com>
2023-09-12 20:14:43 -0300

    Add files via upload

---
+++ exercicio.c
@@ -0,0 +1,5 @@
+/* Desenvolva uma calculadora com as opçoes (soma, subtração, multiplicação, divisão, potencia, raiz quadrada.)
+Utilize as estruturas de condição:
+Switch para a escolha da operação
+IF e ELSE para validar as operações se pode ou não executar
+*/
